0c5291e494c4d6116cf16cd457ed0f954976c1be
Pedro Brandão <phbrandao@outlook.com>
2021-09-21 03:55:28 -0300

    Update imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './components/Home';
 import Photo from './components/Photo';
 import './App.css';
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
         <div className="navbar">
-            <img src="NASA_logo.svg"/>
+            <img src="NASA_logo.svg" alt="NASA logo"/>
             <h1>NASA Astronomy Picture of The Day</h1>
         </div>
     )
